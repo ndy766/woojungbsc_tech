1003b2394a62d4ff1df38f4c44b4188072bba8fc
daeyoung <ndy766@naver.com>
2017-01-26 14:51:28 +0900

    주소록 기능 구현

--- routes/address_book.js
+++ routes/address_book.js
@@ -19,10 +19,133 @@ conn.connect();
 
 
 router.get('/', function(req, res){
+    var name = req.session.user.name;
+    var sql = "SELECT * FROM address_book ORDER BY customer_name";
+    var address_list = [];
 
-    res.render('address_book_list',{});
+    conn.query(sql, function(err, result){
+        address_list = result;
+        res.render('address_book_list',{
+            name:name,
+            address_list:address_list
+        });
+    });
 
 });
+
+router.get('/registerForm', function(req, res){
+    var msg = req.query.msg;
+    (msg!='' && msg!=undefined && msg!=null)?msg=msg:msg='';
+
+    var sql = "SELECT * FROM customer ORDER BY name";
+    var customerList = [];
+    conn.query(sql, function(err ,result){
+        customerList = result;
+        res.render('address_book_register_form', {
+            customerList:customerList,
+            msg:msg
+        });
+    })
+
+});
+
+router.post('/register', function(req, res){
+    var name = req.body.name;
+    var phone = req.body.phone;
+    var email = req.body.email;
+    var customer_name = req.body.customer_name;
+    var msg="";
+    var address = {
+        name:name,
+        phone:phone,
+        email:email,
+        customer_name:customer_name
+    };
+    var sql =  "INSERT INTO address_book SET ?";
+    conn.query(sql, address, function(err, result){
+        msg ='등록 성공';
+        res.redirect('/addressBook/registerForm?msg='+msg);
+    });
+});
+
+router.get('/delete', function(req, res){
+    var email = req.query.email;
+    var sql = "DELETE FROM address_book WHERE email='"+email+"'";
+    conn.query(sql, function(err, result){
+        res.redirect('/addressBook');
+
+    })
+});
+
+
+//수정폼
+router.get('/modifyForm', function(req, res){
+    var msg = req.query.msg;
+    (msg!='' && msg!=undefined && msg!=null)?msg=msg:msg='';
+
+
+    var email = req.query.email;
+    var address = {};
+    var sql = "SELECT * FROM address_book WHERE email='"+email+"'";
+    conn.query(sql, function(err, result){
+        address = result[0];
+
+        var sql2 = "SELECT * FROM customer ORDER BY name";
+        var customerList = [];
+        conn.query(sql2, function(err ,result){
+            customerList = result;
+            res.render('address_book_modify_form', {
+                customerList:customerList,
+                msg:msg,
+                address:address
+            });
+        })
+
+    });
+});
+
+
+//수정하기
+router.post('/modify', function(req, res){
+    var name = req.body.name;
+    var phone = req.body.phone;
+    var email = req.body.email;
+    var customer_name = req.body.customer_name;
+    var msg="";
+    var address = {
+        email:email,
+        name:name,
+        phone:phone,
+        customer_name:customer_name
+    };
+    console.log(address);
+    var sql =  "UPDATE address_book SET ? WHERE email='"+email+"'";
+    conn.query(sql, address, function(err, result){
+        msg ='수정 성공';
+        res.redirect('/addressBook/modifyForm?msg='+msg+'&email='+email);
+    });
+});
+
+
+//검색 기능
+router.post('/search', function (req, res) {
+    var name = req.session.user.name;
+    var searchType = req.body.searchType;
+    var keyword = req.body.keyword;
+    var address_list = [];
+    var sql = "SELECT * FROM address_book WHERE " + searchType + " LIKE '%" + keyword + "%' ORDER BY name DESC";
+
+    conn.query(sql, function(err, result){
+        address_list = result;
+        res.render('address_book_list', {
+            name:name,
+            address_list:address_list
+        });
+    });
+
+
+});
+
 
 
 
