c89193ed2be61b94588d076d9754f6d74078048d
daeyoung <ndy766@naver.com>
2017-02-17 10:15:36 +0900

    직원에 김진우 이사님 임원으로 추가

--- util/memberPositionCheck.js
+++ util/memberPositionCheck.js
@@ -4,12 +4,13 @@
 exports.getMemberPosition = function(rank){
     var position = '사원';
 
-    if(rank=='0') position='부장';
-    else if(rank=='1') position='차장';
-    else if(rank=='2') position='과장';
-    else if(rank=='3') position='대리';
-    else if(rank=='4') position='주임';
-    else if(rank=='5') position='사원';
+    if(rank=='0') position='임원';
+    else if(rank=='1') position='부장';
+    else if(rank=='2') position='차장';
+    else if(rank=='3') position='과장';
+    else if(rank=='4') position='대리';
+    else if(rank=='5') position='주임';
+    else if(rank=='6') position='사원';
     else position='기타';
 
     return position;
